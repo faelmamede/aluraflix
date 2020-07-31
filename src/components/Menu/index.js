003022c33154bd2f2d1ab57c217b7e7a5b969fc7
Rafael Mamede <r.silvamamede@gmail.com>
2020-07-31 13:40:00 -0300

    Creation of some SPA pages. Carousel's Improvement

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import Logo from './../../assets/img/logo.png';
-import './Menu.css'
+import './Menu.css';
 import Button from '../Button';
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="AluraFlix Logo"></img>
-            </a>
+            </Link>
 
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo Vídeo
             </Button>
         </nav>
